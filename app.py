0257ca96092a5bdcf54746450669d80cbaa9d294
snehith57624 <snehithb57624@gmail.com>
2023-05-27 16:33:41 -0500

    flask server

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+from colorama import Fore, Style
+
+from constants import banner
+from interface import ToucanStrikeInterface
+
+app = Flask(__name__)
+terminal = ToucanStrikeInterface()
+
+@app.route('/')
+def index():
+    return render_template('index.html', banner=Fore.YELLOW + banner + Style.RESET_ALL)
+
+@app.route('/command', methods=['POST'])
+def execute_command():
+    command = request.form.get('command')
+    output = terminal.onecmd(command)
+    return output
+
+if __name__ == '__main__':
+    app.run(debug=True)
