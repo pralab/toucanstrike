8215e8d841a18e8976cb621502c18d5280e5a94a
snehith57624 <snehithb57624@gmail.com>
2023-05-29 14:14:50 -0500

    upload api

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from colorama import Fore, Style
 
 from constants import banner
@@ -18,6 +18,21 @@ def execute_command():
     output = terminal.onecmd(command)
     print("output ",output)
     return output
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file uploaded'})
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'})
+
+    # Save the file to the static folder
+    file.save(os.path.join(app.static_folder, file.filename))
+
+    return jsonify({'message': 'File uploaded successfully'})
 
 if __name__ == '__main__':
     app.run(debug=True)
