8052f097958dafd410b96b159a5608f0c1d13fe9
snehith57624 <snehithb57624@gmail.com>
2023-05-29 14:05:34 -0500

    debugging

--- app.py
+++ app.py
@@ -14,7 +14,9 @@ def index():
 @app.route('/command', methods=['POST'])
 def execute_command():
     command = request.form.get('command')
+    print("input ",command)
     output = terminal.onecmd(command)
+    print("output ",output)
     return output
 
 if __name__ == '__main__':
