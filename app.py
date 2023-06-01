fc0565ebd4335ebbf4c0edebed0b7328dc7e4962
snehith57624 <snehithb57624@gmail.com>
2023-06-01 10:44:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,21 +5,23 @@ from constants import banner
 from interface import ToucanStrikeInterface
 import os
 
-
 app = Flask(__name__)
 terminal = ToucanStrikeInterface()
+
 
 @app.route('/')
 def index():
     return "working"
 
+
 @app.route('/command', methods=['POST'])
 def execute_command():
     command = request.form.get('command')
-    print("input ",command)
+    print("input ", command)
     output = terminal.onecmd(command)
-    print("output ",output)
+    print("output ", output)
     return output
+
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -31,14 +33,27 @@ def upload_file():
     if file.filename == '':
         return jsonify({'error': 'No selected file'})
 
+    target = request.form.get('target')
+
+    if target is None:
+        return jsonify({'error': 'Invalid request parameters, target not found'})
+    target_command = "target " + target
+    output = terminal.onecmd(target_command)
+    print("target_command ", output)
+
     # Save the file to the static folder
     file.save(os.path.join(app.static_folder, file.filename))
-    command = "data "+str(os.path.join(app.static_folder, file.filename))
-    print("input ",command)
+    command = "data " + str(os.path.join(app.static_folder, file.filename))
     output = terminal.onecmd(command)
-    print("output ",output)
+    print("file ", output)
 
-    return jsonify({'message': 'File uploaded successfully'})
+    if target is None or command is None:
+        return jsonify({'error': 'Invalid request parameters'})
+
+    output = terminal.onecmd("run")
+    print("output ", output)
+    return output
+
 
 if __name__ == '__main__':
     app.run(debug=True)
