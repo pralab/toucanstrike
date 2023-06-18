3ef58ec351cbcaf377da0c6e15eba0e246388c9a
snehith57624 <snehithb57624@gmail.com>
2023-06-18 15:19:26 -0500

    refactoring

--- app.py
+++ app.py
@@ -33,22 +33,11 @@ def upload_file():
     if file.filename == '':
         return jsonify({'error': 'No selected file'})
 
-    target = request.form.get('target')
-
-    # if target is None:
-    #     return jsonify({'error': 'Invalid request parameters, target not found'})
-    target_command = "target " + target
-    output = terminal.onecmd(target_command)
-    print("target_command ", output)
-
     # Save the file to the static folder
     file.save(os.path.join(app.static_folder, file.filename))
     command = "data " + str(os.path.join(app.static_folder, file.filename))
     output = terminal.onecmd(command)
     print("file ", output)
-
-    if target is None or command is None:
-        return jsonify({'error': 'Invalid request parameters'})
 
     output = terminal.onecmd("run")
     print("output ", output)
